af0d6d5cbecb6226acfb9756352cc15f89ffc2c5
Teddy <teddybrian543@gmail.com>
2025-11-17 21:33:45 +0300

    Edit Admin Dashboard

--- src/routes/medicineRoutes.js
+++ src/routes/medicineRoutes.js
@@ -6,14 +6,14 @@ import {
     updateMedicine, 
     deleteMedicine 
 } from '../controllers/medicineController.js';
-import { protect, adminOnly } from '../middleware/auth.js';
+import { protect, adminOnly , adminOrPharmacist } from '../middleware/auth.js';
 
 const router = express.Router();
 
 // Only logged-in admin can add medicine
-router.post('/add', protect, adminOnly,upload.single("image"), addMedicine);
-router.put("/:id", protect, adminOnly,upload.single("image"), updateMedicine);
-router.delete("/:id", protect, adminOnly, deleteMedicine);
+router.post('/add', protect, adminOrPharmacist,upload.single("image"), addMedicine);
+router.put("/:id", protect, adminOrPharmacist,upload.single("image"), updateMedicine);
+router.delete("/:id", protect, adminOrPharmacist, deleteMedicine);
 router.get("/", protect, getMedicines);
 
 export default router;
